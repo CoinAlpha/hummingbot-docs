562b561faf8ac43b43eb825f100d52a639e62fba
Jeff Reiner <gone@hey.com>
2020-09-14 13:39:25 +0200

    Swap to teal as its closer to hummingbot brand color
    Maybe just use a grey?

--- src/gatsby-theme-apollo-core/utils/colors.js
+++ src/gatsby-theme-apollo-core/utils/colors.js
@@ -1,10 +1,22 @@
 import { colors as coreColors } from "gatsby-theme-apollo-core/src/utils/colors";
 import { colors as spaceKitColors } from "@apollo/space-kit/colors";
 
+/**
+ * @todo Find a way to hook the brand colors up to this
+ */
+const hummingbot = {
+  darkest: "#013D54",
+  darker: "#04596E",
+  dark: "#087887",
+  base: "#0D999E",
+  light: "#4ABBB6",
+  lighter: "#8AD6CC",
+  lightest: "#CCEFE7",
+};
+
 export const colors = {
   ...coreColors,
-  primary: spaceKitColors.green.dark,
-  primaryLight: spaceKitColors.green.light,
-  primaryDark: spaceKitColors.green.darkest,
-  secondary: spaceKitColors.green.dark,
+  primary: spaceKitColors.teal.dark,
+  primaryLight: spaceKitColors.teal.base,
+  primaryDark: spaceKitColors.teal.dark,
 };

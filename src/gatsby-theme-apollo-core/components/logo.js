7b9d81fc5b95a170a6bccc602e2d1a1c814f506c
Jeff Reiner <gone@hey.com>
2020-09-14 14:01:59 +0200

    Adding better custom logo

--- src/gatsby-theme-apollo-core/components/logo.js
+++ src/gatsby-theme-apollo-core/components/logo.js
@@ -1,5 +1,15 @@
-import React from "react";
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
+import { colors } from "gatsby-theme-apollo-core";
+import logo from "../../images/brand-logo.png";
 
 export default function Logo() {
-  return <div>Hummingbot Docs</div>;
+  return (
+    <div css={{ display: "flex", alignItems: "center" }}>
+      <img src={logo} alt="" css={{ height: 24, width: 24 }} />
+      <div css={{ marginLeft: 8, fontWeight: "bold" }}>
+        Hummingbot <span css={{ color: colors.primary }}>Docs</span>
+      </div>
+    </div>
+  );
 }

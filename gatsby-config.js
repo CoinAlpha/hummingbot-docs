4b6fe916a4020c354e50407b42d5e5fd51f16f36
carlolm <carlo@hummingbot.io>
2020-10-21 22:56:26 -0700

    (fix) algolia indexing

--- gatsby-config.js
+++ gatsby-config.js
@@ -240,9 +240,6 @@ module.exports = {
         indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
         queries,
         chunkSize: 10000,
-        enablePartialUpdates: true, // default: false
-        matchFields: ["slug", "modified"], // Array<String> default: ['modified']
-        concurrentQueries: false, // default: true
       },
     },
   ],

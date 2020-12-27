aeea91e2e8287512e896d9296b1795e3537684c6
JeremyKono <73882610+JeremyKono@users.noreply.github.com>
2020-12-27 10:49:03 +0800

    (doc) added strategy autostart page

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,7 @@ const sidebarCategories = {
     "operation/docker-commands",
     "operation/adv-command-ref",
     "operation/debug",
+    "operation/strategy-autostart",
   ],
   Features: [
     "features/paper-trade",
@@ -59,10 +60,7 @@ const sidebarCategories = {
     "protocol-connectors/terra",
     "protocol-connectors/uniswap",
   ],
-  "Hummingbot Gateway": [
-    "gateway/overview",
-    "gateway/installation",
-  ],
+  "Hummingbot Gateway": ["gateway/overview", "gateway/installation"],
   Strategies: [
     "strategies/overview",
     "strategies/pure-market-making",

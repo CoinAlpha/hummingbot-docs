2addf7b661c7ab1953f38739e6d506799892a80d
JeremyKono <73882610+JeremyKono@users.noreply.github.com>
2020-12-29 08:24:18 +0800

    (doc) Moved to advanced market making

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,6 @@ const sidebarCategories = {
     "features/performance-history",
     "features/kill-switch",
     "features/telegram",
-    "features/max-order-age",
   ],
   "Exchange Connectors": [
     "exchange-connectors/overview",
@@ -84,6 +83,7 @@ const sidebarCategories = {
     "strategies/add-transaction-costs",
     "strategies/external-price-source",
     "strategies/order-override",
+    "strategies/max-order-age",
   ],
   Scripts: ["scripts/overview", "scripts/script-base"],
   "Developing Connectors": [

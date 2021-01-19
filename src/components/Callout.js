2e2ed41ecc7dc732ed0c257fb290cdbb13e29cd5
mikey084 <lium039@gmail.com>
2021-01-19 09:54:33 -0800

    (docs) fix css add react icons

--- src/components/Callout.js
+++ src/components/Callout.js
@@ -1,55 +1,63 @@
 import React from "react";
 import "./Callout.css";
 
+import { FcInfo } from "react-icons/fc";
+import { AiFillBug } from "react-icons/ai";
+import { FcApproval } from "react-icons/fc";
+import { FcHighPriority } from "react-icons/fc";
+import { FcMenu } from "react-icons/fc";
+import { FcCancel } from "react-icons/fc";
+import { FcMediumPriority } from "react-icons/fc";
+
 export default function Callout(props) {
+  var calloutType = "callout" + props.type;
   const Colors = {
     success: {
       heading: "SUCCESS:",
       color: "#707070",
       backgroundColor: "#E4FFE5",
       borderColor: "#4FB916",
-      icon: "✅",
+      icon: <FcApproval />,
     },
     warning: {
       heading: "WARNING:",
       color: "#a94442",
       backgroundColor: "#f2dede",
       borderColor: "#a94442",
-      icon: "❗",
+      icon: <FcHighPriority />,
     },
     danger: {
       heading: "DANGER:",
       color: "#707070",
       backgroundColor: "#FEFFDC",
       borderColor: "#C6C918",
-      icon: "🚧",
+      icon: <FcMediumPriority />,
     },
     info: {
       heading: "INFO:",
       color: "#707070",
       backgroundColor: "#E7F6FF",
       borderColor: "#1A96CB",
-      icon: "📘",
+      icon: <FcInfo />,
     },
     bug: {
       heading: "BUG:",
       color: "#707070",
       backgroundColor: "#FFF2DC",
       borderColor: "#BB8C2D ",
-      icon: "🐞",
+      icon: <AiFillBug />,
     },
-    quote: {
-      heading: "QUOTE: ",
+    tip: {
+      heading: "Tip: ",
       color: "#707070",
       backgroundColor: "#EEEEEE",
       borderColor: "#8E8E8E",
-      icon: "💬",
+      icon: <FcMenu />,
     },
   };
   const Style = {
-    padding: "15px",
     borderLeft: "5px solid ",
-    borderColor: "transparent",
+    borderColor: "black",
     marginBottom: "20px",
     borderRadius: "4px",
     color: Colors[props.type].color,
@@ -93,13 +101,26 @@ export default function Callout(props) {
   return (
     <table>
       <tr>
-        <td style={Style}>
-          <b>
-            {Colors[props.type].icon} {Colors[props.type].heading}
-          </b>
-          <br />
-          <span /> {buildBody(props.body)}
-          <a href={props.link}>&nbsp;{props.linkName}.</a>
+        <td>
+          <div style={Style}>
+            <div className="callout-top">
+              <b>
+                <span className="callout-icon">
+                  {" "}
+                  {Colors[props.type].icon}{" "}
+                </span>
+                &nbsp;{" "}
+                <span className="callout-text">
+                  {" "}
+                  {Colors[props.type].heading}
+                </span>
+              </b>
+            </div>
+            <div className={"callout-bottom " + calloutType}>
+              <span /> {buildBody(props.body)}
+              <a href={props.link}>&nbsp;{props.linkName}.</a>
+            </div>
+          </div>
         </td>
       </tr>
     </table>

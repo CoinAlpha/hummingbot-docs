e24dfe60a4f06ab14ae1bbc55d11a48ef5bb7ebb
JeremyKono <jeremy@hummingbot.io>
2021-03-08 01:24:05 +0800

    (doc) spot perpetual arbitrage strategy

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,6 +71,7 @@ const sidebarCategories = {
     "strategies/celo-arb",
     "strategies/amm-arb",
     "strategies/liquidity-mining",
+    "strategies/spot-perpetual-arb",
   ],
   "Advanced Market Making": [
     "strategies/adv-market-making",

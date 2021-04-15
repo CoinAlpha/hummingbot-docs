14bb0e42654e5756e38e574b39b62e927e1c1929
dendenski <dennisbuyco@gmail.com>
2021-04-15 13:06:36 +0800

    doc / release-notes-v0.38.1

--- gatsby-config.js
+++ gatsby-config.js
@@ -120,6 +120,7 @@ const sidebarCategories = {
   ],
   "Release Notes": [
     "release-notes/overview",
+    "release-notes/0.38.1",
     "release-notes/0.38.0",
     "release-notes/0.37.1",
     "release-notes/0.37.0",

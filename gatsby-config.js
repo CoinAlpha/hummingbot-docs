244827ba809baffeab3711e6db73ac983048d3cc
Ralph Emerson <ralph.escoto@hummingbot.io>
2021-05-26 19:49:41 +0800

    doc/cleanup_cheatsheet

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,6 @@ const sidebarCategories = {
     "features/rate-oracle",
   ],
 
-  "Cheat Sheet": ["cheat-sheet/command-ref", "cheat-sheet/docker-commands"],
   "Spot Connectors": [
     "spot-connectors/overview",
     "spot-connectors/ascend-ex",

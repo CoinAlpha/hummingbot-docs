99ad6d8f7f571f4fa29606fd1e127e4152e2dba3
Ralph Emerson <ralph.escoto@hummingbot.io>
2021-06-09 17:55:26 +0800

    cleanup resources grammar check

--- gatsby-config.js
+++ gatsby-config.js
@@ -196,7 +196,7 @@ const sidebarCategories = {
   Resources: [
     "resources/faq",
     "resources/glossary",
-    "[Hummingbot YouTube](https://www.youtube.com/c/HummingbotChannel)",
+    "[Hummingbot YouTube](https://www.youtube.com/c/Hummingbot)",
     "[Hummingbot whitepaper](https://hummingbot.io/hummingbot.pdf)",
   ],
   Troubleshooting: [

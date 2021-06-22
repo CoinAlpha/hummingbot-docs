1243cdf4c1142689038e5cc81746754bbd9bf711
Ralph Emerson <ralph.escoto@hummingbot.io>
2021-06-23 00:11:43 +0800

    doc/release-notes-v0.41

--- gatsby-config.js
+++ gatsby-config.js
@@ -138,6 +138,7 @@ const sidebarCategories = {
   ],
   "Release Notes": [
     "release-notes/overview",
+    "release-notes/0.41.0",
     "release-notes/0.40.0",
     "release-notes/0.39.1",
     "release-notes/0.39.0",

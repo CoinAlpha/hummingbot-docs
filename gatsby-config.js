72bc4835931392d75d12f06c4658f61243dc9fcb
Ralph Emerson Escoto <ralphemersonic@gmail.com>
2021-07-25 14:22:40 +0800

    doc/release-notes-v0.42

--- gatsby-config.js
+++ gatsby-config.js
@@ -140,6 +140,7 @@ const sidebarCategories = {
   ],
   "Release Notes": [
     "release-notes/overview",
+    "release-notes/0.42.0",
     "release-notes/0.41.0",
     "release-notes/0.40.0",
     "release-notes/0.39.1",
